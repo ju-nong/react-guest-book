398f13ee7ea91a29795aa8bcc65541dddc8d6f2e
이준용 <iiiuooyoo@gmail.com>
2023-09-19 16:34:54 +0900

    feat : Firebase 연동

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import firebase from "./firebase";
+
+console.log(firebase);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
