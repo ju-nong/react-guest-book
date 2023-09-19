e3f7bd1de185b1869c5343fcdafaa47a043bc82b
이준용 <iiiuooyoo@gmail.com>
2023-09-19 17:35:30 +0900

    feat : AccountModal 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useState, useEffect } from "react";
+
+import { authService } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+import { AccountModal } from "./components";
+
 function App() {
-    return <>Hello World!</>;
+    const [isLogin, setIsLogin] = useState(false);
+
+    useEffect(() => {
+        onAuthStateChanged(authService, (user) => {
+            setIsLogin(!!user);
+        });
+    }, []);
+
+    return (
+        <>
+            Hello World!
+            <AccountModal />
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/AccountModal.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const AccountModalStyled = styled.div`
+    position: relative;
+    border-radius: 0.5rem;
+    box-shadow: 0 0 4px 0 rgba(17, 22, 26, 0.16),
+        0 2px 4px 0 rgba(17, 22, 26, 0.08), 0 4px 8px 0 rgba(17, 22, 26, 0.08);
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 80%;
+    max-width: 400px;
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    row-gap: 1rem;
+
+    @media screen and (max-width: 466px) {
+        padding: 2rem 1rem;
+    }
+`;
+
+const TitleStyled = styled.h1`
+    text-align: center;
+
+    @media screen and (max-width: 466px) {
+        font-size: 6.5vw;
+    }
+`;
+
+const DescriptionStyled = styled.p`
+    text-align: center;
+
+    @media screen and (max-width: 466px) {
+        font-size: 3vw;
+    }
+`;
+
+const ButtonContainerStyled = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    row-gap: 1rem;
+    padding-top: 2rem;
+
+    > button {
+        box-shadow: 0 0 8px 0 rgba(17, 22, 26, 0.16),
+            0 4px 8px 0 rgba(17, 22, 26, 0.08),
+            0 8px 16px 0 rgba(17, 22, 26, 0.08);
+        cursor: pointer;
+        font-family: Roboto;
+        width: 240px;
+        height: 50px;
+        display: flex;
+        column-gap: 4px;
+        align-items: center;
+        justify-content: center;
+        padding: 6px 24px;
+        font-size: 1rem;
+        color: rgba(75, 85, 99);
+
+        > img {
+            height: 100%;
+        }
+    }
+
+    @media screen and (max-width: 400px) {
+        > button {
+            font-size: 4vw;
+        }
+    }
+`;
+
+const GoogleAccountButtonStyled = styled.button`
+    background-color: #fff;
+`;
+
+const GithubAccountButtonStyled = styled.button`
+    background-color: #fff;
+`;
+
+function AccountModal() {
+    return (
+        <AccountModalStyled>
+            <TitleStyled>react-guest-book</TitleStyled>
+            <DescriptionStyled>
+                방명록을 사용하시려면 로그인을 해주세요.
+            </DescriptionStyled>
+            <ButtonContainerStyled>
+                <GoogleAccountButtonStyled>
+                    <img src="/images/google-logo.png" alt="Google Logo" />
+                    Sign in with Google
+                </GoogleAccountButtonStyled>
+                <GithubAccountButtonStyled>
+                    <img src="/images/github-logo.png" alt="Github Logo" />
+                    Sign in with Github
+                </GithubAccountButtonStyled>
+            </ButtonContainerStyled>
+        </AccountModalStyled>
+    );
+}
+
+export { AccountModal };
