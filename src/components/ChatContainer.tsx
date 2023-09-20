1bed02fdac6a448eb3d66b0911599f89dd026900
이준용 <iiiuooyoo@gmail.com>
2023-09-20 11:38:51 +0900

    feat : [ChatList] 최신 및 이전 기록 가져오는거 구현

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -1,18 +1,14 @@
 import {} from "react";
 import styled from "@emotion/styled";
 
-import { authService } from "../firebase";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
-import { ChatInput } from ".";
+import { ChatList, ChatInput } from ".";
 
 function ChatContainer() {
-    const db = getFirestore();
-
     return (
-        <div>
-            <ul></ul>
+        <main>
+            <ChatList />
             <ChatInput />
-        </div>
+        </main>
     );
 }
 
--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,7 +1,85 @@
-import React from "react";
+import { useEffect, useState, useRef } from "react";
+
+import {
+    Timestamp,
+    getFirestore,
+    query,
+    collection,
+    orderBy,
+    limit,
+    getDocs,
+    startAfter,
+    QuerySnapshot,
+} from "firebase/firestore";
+import { authService } from "../firebase";
+
+type Chat = {
+    id: string;
+    createAt: Timestamp;
+    email: string;
+    name: string;
+    profile: string;
+    text: string;
+};
 
 function ChatList() {
-    return <ul></ul>;
+    const db = getFirestore();
+
+    const currentDocument = useRef<null | QuerySnapshot>(null);
+    const [chats, setChats] = useState<Chat[]>([]);
+
+    useEffect(() => {
+        getChats(true);
+    }, []);
+
+    async function getChats(init = false) {
+        let chatQuery = null;
+
+        if (init) {
+            chatQuery = query(
+                collection(db, "chat"),
+                orderBy("createAt", "desc"),
+                limit(20),
+            );
+        } else {
+            if (currentDocument.current)
+                chatQuery = query(
+                    collection(db, "chat"),
+                    orderBy("createAt", "desc"),
+                    startAfter(currentDocument.current.docs.at(-1)),
+                    limit(20),
+                );
+        }
+
+        const cacheDocument = currentDocument.current;
+
+        currentDocument.current = await getDocs(chatQuery!);
+        const { docs } = currentDocument.current;
+
+        if (docs.length) {
+            setChats((chats) => [
+                ...docs.map(
+                    (doc) => ({ id: doc.id, ...doc.data() } as unknown as Chat),
+                ),
+                ...chats,
+            ]);
+        } else {
+            currentDocument.current = cacheDocument;
+        }
+    }
+
+    function handleClick() {
+        getChats();
+    }
+
+    return (
+        <ul>
+            <button onClick={handleClick}>이전꺼</button>
+            {chats.map((chat) => (
+                <li key={chat.id}>{chat.text}</li>
+            ))}
+        </ul>
+    );
 }
 
 export { ChatList };
