5cded8e67bde571d8e2432689b41d2fa41dee2f6
이준용 <iiiuooyoo@gmail.com>
2023-09-20 15:35:08 +0900

    feat : dayjs 추가 및 지역 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import "./index.css";
 
 import firebase from "./firebase";
 
+import dayjs from "dayjs";
+dayjs.locale("ko");
+
 console.log(firebase);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
