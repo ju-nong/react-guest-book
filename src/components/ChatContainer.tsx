10f5392bc19e0286bda6449f914b1c8769b97b83
이준용 <iiiuooyoo@gmail.com>
2023-09-20 15:59:36 +0900

    style : [ChatConatiner] 좌우 여백 추가

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -1,11 +1,16 @@
 import { ChatList, ChatInput } from ".";
+import styled from "@emotion/styled";
+
+const ChatContainerStyled = styled.main`
+    padding: 0px 16px;
+`;
 
 function ChatContainer() {
     return (
-        <main>
+        <ChatContainerStyled>
             <ChatList />
             <ChatInput />
-        </main>
+        </ChatContainerStyled>
     );
 }
 
