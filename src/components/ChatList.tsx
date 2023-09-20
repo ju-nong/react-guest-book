9b4bbd95bea138cb2853ab2c267cb8fb1b14882c
이준용 <iiiuooyoo@gmail.com>
2023-09-20 14:07:39 +0900

    feat : [ChatList] 이전, onSnapshot 프로토타입

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -9,9 +9,10 @@ import {
     limit,
     getDocs,
     startAfter,
-    QuerySnapshot,
+    onSnapshot,
+    QueryDocumentSnapshot,
+    DocumentData,
 } from "firebase/firestore";
-import { authService } from "../firebase";
 
 type Chat = {
     id: string;
@@ -22,61 +23,112 @@ type Chat = {
     text: string;
 };
 
+type QueryType = "basic" | "before";
+
 function ChatList() {
     const db = getFirestore();
+    const LIMIT = 20;
 
-    const currentDocument = useRef<null | QuerySnapshot>(null);
+    const beforeDocument = useRef<null | QueryDocumentSnapshot<DocumentData>>(
+        null,
+    );
     const [chats, setChats] = useState<Chat[]>([]);
 
     useEffect(() => {
-        getChats(true);
+        const unsubscribe = onSnapshot(getQuery("basic"), (snapshot) => {
+            const changes = snapshot.docChanges();
+            const reversedChanges = changes.reverse();
+
+            // init
+            if (!beforeDocument.current) {
+                setChats((chats) => [
+                    ...chats,
+                    ...reversedChanges.map(
+                        (change) =>
+                            ({
+                                id: change.doc.id,
+                                ...change.doc.data(),
+                            } as Chat),
+                    ),
+                ]);
+
+                beforeDocument.current = reversedChanges[0].doc;
+            } else {
+                // 추가된 것이 있으면
+                const newChats: DocumentData[] = [];
+
+                for (let i = 0; i < reversedChanges.length; i++) {
+                    const { type, doc } = reversedChanges[i];
+
+                    if (type === "added") {
+                        newChats.push(doc);
+                    }
+                }
+
+                setChats((chats) => [
+                    ...chats,
+                    ...newChats.map(
+                        (chat) =>
+                            ({
+                                id: chat.id,
+                                ...chat.data(),
+                            } as Chat),
+                    ),
+                ]);
+            }
+        });
+
+        return () => {
+            unsubscribe();
+        };
     }, []);
 
-    async function getChats(init = false) {
-        let chatQuery = null;
-
-        if (init) {
-            chatQuery = query(
-                collection(db, "chat"),
-                orderBy("createAt", "desc"),
-                limit(20),
-            );
-        } else {
-            if (currentDocument.current)
-                chatQuery = query(
+    function getQuery(type: QueryType) {
+        switch (type) {
+            case "basic":
+                return query(
                     collection(db, "chat"),
                     orderBy("createAt", "desc"),
-                    startAfter(currentDocument.current.docs.at(-1)),
-                    limit(20),
+                    limit(LIMIT),
                 );
-        }
-
-        const cacheDocument = currentDocument.current;
-
-        currentDocument.current = await getDocs(chatQuery!);
-        const { docs } = currentDocument.current;
-
-        if (docs.length) {
-            setChats((chats) => [
-                ...docs.map(
-                    (doc) => ({ id: doc.id, ...doc.data() } as unknown as Chat),
-                ),
-                ...chats,
-            ]);
-        } else {
-            currentDocument.current = cacheDocument;
+            case "before":
+                return query(
+                    collection(db, "chat"),
+                    orderBy("createAt", "desc"),
+                    startAfter(beforeDocument.current),
+                    limit(LIMIT),
+                );
         }
     }
 
-    function handleClick() {
-        getChats();
+    // 이전 채팅 기록 가져오기
+    async function getBeforeChat() {
+        const chatQuery = getQuery("before");
+        const { docs } = await getDocs(chatQuery!);
+
+        if (docs.length) {
+            const reversedBeforeChats = docs.reverse();
+
+            setChats((chats) => [
+                ...reversedBeforeChats.map(
+                    (doc) => ({ id: doc.id, ...doc.data() } as Chat),
+                ),
+                ...chats,
+            ]);
+
+            beforeDocument.current = reversedBeforeChats[0];
+        } else {
+            alert("마지막입니다");
+        }
     }
 
     return (
         <ul>
-            <button onClick={handleClick}>이전꺼</button>
+            <button onClick={getBeforeChat}>이전꺼</button>
             {chats.map((chat) => (
-                <li key={chat.id}>{chat.text}</li>
+                <li key={chat.id}>
+                    {chat.name}: {chat.text}
+                </li>
             ))}
         </ul>
     );
